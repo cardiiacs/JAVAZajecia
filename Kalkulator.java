a8ba49769798eb9352633324eca8d02710177945
cardiiacs <116751218+cardiiacs@users.noreply.github.com>
2022-12-03 16:09:33 +0100

    Add files via upload

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,30 +1,74 @@
-package zajecia;
+package javazajecia;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Kalkulator {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Podaj pierwszą liczbę:");
 
-        System.out.println("Podaj znak działania:");
+            float l1 = scanner.nextInt();
 
-        String znak = scanner.next();
+            System.out.println("Podaj drugą liczbę:");
 
-        System.out.println("Podaj pierwszą liczbę:");
+            float l2 = scanner.nextInt();
 
-        int l1 = scanner.nextInt();
+            System.out.println("Podaj znak działania:");
+            System.out.println("+ dodawanie");
+            System.out.println("- odejmowanie");
+            System.out.println("* mnożenie");
+            System.out.println("/ dzielenie");
+            System.out.println("% dzielenie modulo");
+            System.out.println("x wyjście z programu");
 
-        System.out.println("Podaj drugą liczbę:");
-
-        int l2 = scanner.nextInt();
-
-        if (znak =="+"); System.out.println(l1 + " + " + l2 + " = " + (l1 + l2));
-
-
-                }
+            char znak = scanner.next().charAt(0);
+            
+            float wynik;
+            
+            switch (znak) {
+            
+            case '+':
+                wynik = l1 + l2;
+                System.out.println(l1 + " + " + l2 + " = " + wynik);
+                break;
+                
+            case '-':
+            	wynik = l1 - l2;
+            	System.out.println(l1 + " - " + l2 + " = " + wynik);
+                break;
+                
+            case '*':
+            	wynik = l1 * l2;
+            	System.out.println(l1 + " * " + l2 + " = " + wynik);
+                break;
+            
+            case '/':
+            	if (l2 != 0) {
+            		wynik = l1 / l2;
+                	System.out.println(l1 + " / " + l2 + " = " + wynik);
+                    break;
+            	}
+            	if (l2 == 0) {
+            		System.out.println("Błąd: dzielenie przez zero");
+            	}
+            	
+            case '%':
+            	wynik = l1 % l2;
+            	System.out.println(l1 + " % " + l2 + " = " + wynik);
+                break;
+                
+            case 'x':
+            	break;
+            	
+            default:
+                System.out.println("Nieprawidłowy znak");
+                break;
+                
             }
-
-
+            scanner.close();
+        }
+}
+         
